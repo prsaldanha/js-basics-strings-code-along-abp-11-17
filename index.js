e613fdc1b5a988bffa9568d2e321fb9f3b4e7759
prem_saldanha@yahoo.ca <prem_saldanha@yahoo.ca>
2017-11-11 19:49:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const currentUser = 'Prem';// Write your code in this file!
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
-let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + uppercase(currentUser) + '!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
